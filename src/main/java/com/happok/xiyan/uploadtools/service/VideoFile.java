eaad80f387881d1ed35c003d233b01614d546ca3
陈阿超 <chenac@happok.com>
2018-09-26 19:16:25 +0800

    nishishei
    Signed-off-by: 陈阿超 <chenac@happok.com>

--- src/main/java/com/happok/xiyan/uploadtools/service/VideoFile.java
+++ src/main/java/com/happok/xiyan/uploadtools/service/VideoFile.java
@@ -56,29 +56,29 @@ public class VideoFile {
 
     @Scheduled(cron = "0 0/2 * * * *")
     public void scheduled1() {
-        if(commonConfig.getSql()){
+        if (commonConfig.getSql()) {
             log.info("查询数据库");
-            if(!StringUtils.isEmpty(datatimeConfig.getBegintime())){
+            if (!StringUtils.isEmpty(datatimeConfig.getBegintime())) {
                 String end_time;
-                if (StringUtils.isEmpty(datatimeConfig.getEndtime())){
+                if (StringUtils.isEmpty(datatimeConfig.getEndtime())) {
                     log.info("结束时间为空");
                     SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                     end_time = df.format(new Date());
-                }else {
+                } else {
                     end_time = datatimeConfig.getEndtime();
                 }
-                List<BasicInfoEntity> pathEntities = pathMapper.getResourcePath(datatimeConfig.getBegintime(),end_time);
+                List<BasicInfoEntity> pathEntities = pathMapper.getResourcePath(datatimeConfig.getBegintime(), end_time);
                 log.info("数据总条数 = " + pathEntities.size());
 
-                for (BasicInfoEntity item : pathEntities){
+                for (BasicInfoEntity item : pathEntities) {
                     try {
-                        if (StringUtils.isEmpty(item.getAuthor())){
+                        if (StringUtils.isEmpty(item.getAuthor())) {
                             item.setAuthor("北京汉博");
                         }
-                        if (sourceMapper.addSourceInfoOne(item)){
+                        if (sourceMapper.addSourceInfoOne(item)) {
                             log.info("添加成功 " + item.toString());
                         }
-                    }catch (DuplicateKeyException exc){
+                    } catch (DuplicateKeyException exc) {
                         log.info("重复添加，跳过");
                         continue;
                     }
@@ -89,17 +89,28 @@ public class VideoFile {
         // 单个取出来
         log.info("开始");
         SourceEntity sourceEntity = sourceMapper.getSourceInfo();
-        if(ObjectUtils.isEmpty(sourceEntity)){
+        if (ObjectUtils.isEmpty(sourceEntity)) {
             log.info("---> null");
-            return ;
+            return;
         }
 
-        try{
+        try {
             String filePath = pathConfig.getRoot() + sourceEntity.getPath_name();
+            File file = new File(filePath);
+            if (!file.exists()) {
+                log.info("文件不存在:" + filePath);
+                sourceMapper.updateSourceUpload(sourceEntity.getId());
+                return;
+            }
+            if (file.length() == 0) {
+                log.info("文件大小为0KB:" + filePath);
+                sourceMapper.updateSourceUpload(sourceEntity.getId());
+                return;
+            }
             log.info(filePath);
-            String str = formUpload(commonConfig.getUrl(),filePath,"video/mp4");
+            String str = formUpload(commonConfig.getUrl(), filePath, "video/mp4");
             log.info(str);
-            if(!StringUtils.isEmpty(str)) {
+            if (!StringUtils.isEmpty(str)) {
                 log.info("上传资源成功,路径 = " + sourceEntity.getPath_name());
                 JSONObject jsonObject = JSON.parseObject(str);
                 sourceEntity.setPath_name(jsonObject.getString("result"));
@@ -116,34 +127,34 @@ public class VideoFile {
                         user = mapper.readValue(userInfo, UserInfo.class);
                     }
                     JSONObject requestBody = new JSONObject();
-                    requestBody.put("userId",user.getId());
-                    JSONObject json = resourceCenter.addAssessorUser( commonConfig.getBase64(),requestBody);
+                    requestBody.put("userId", user.getId());
+                    JSONObject json = resourceCenter.addAssessorUser(commonConfig.getBase64(), requestBody);
 
-                    if(json.getInteger("code").equals(200)){
+                    if (json.getInteger("code").equals(200)) {
                         log.info("添加免审核用户成功");
                     }
                     SharedEntity sharedEntity = new SharedEntity();
                     sharedEntity.setShared("public");
-                    sharedEntity.setSource_id(sourceId+"");
+                    sharedEntity.setSource_id(sourceId + "");
                     // 添加公开资源
                     JSONObject shareObj = resourceCenter.adSharedSource(commonConfig.getBase64(), sharedEntity);
-                    if(shareObj.getInteger("code").equals(200)){
+                    if (shareObj.getInteger("code").equals(200)) {
                         log.info("添加公开资源成功");
                     }
                     sourceMapper.updateSourceUpload(sourceEntity.getId());
                     log.info("完成");
                 }
-            }else{
+            } else {
                 log.info("路径" + str.toString());
             }
-        }catch (FeignException exc){
+        } catch (FeignException exc) {
             log.error("接口调用异常 ：" + exc.toString());
-        }catch (Exception exc){
+        } catch (Exception exc) {
             log.error("异常 " + exc.toString());
         }
     }
 
-    public static String formUpload(String urlStr,String path,String contentType) {
+    public static String formUpload(String urlStr, String path, String contentType) {
         String res = "";
         HttpURLConnection conn = null;
         // boundary就是request头和上传文件内容的分隔符
